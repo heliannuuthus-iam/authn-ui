c492b31d4052fe75f0f39b42accdec3d73b0cfe9
heliannuuthus <heliannuuthus@gmail.com>
2023-08-11 00:23:44 +0800

    good job

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import React from "react";
 import { Button } from "antd";
+import LoginView from "./components/auth";
 
 const App = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <LoginView style={{ width: 400 , textAlign: "left"}} />
   </div>
 );
 
